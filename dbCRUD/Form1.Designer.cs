5fa83dcc76b159d9d71cc18cd29ffd705f79c43a
Muhammad Suleman <88381507+supolaris@users.noreply.github.com>
2023-11-25 22:49:58 +0500

    Database CRUD

---
+++ dbCRUD/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace dbCRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.insertButton = new System.Windows.Forms.Button();
+            this.regText = new System.Windows.Forms.TextBox();
+            this.nameText = new System.Windows.Forms.TextBox();
+            this.classText = new System.Windows.Forms.TextBox();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.updateButton = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.idText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.idSearchText = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // insertButton
+            // 
+            this.insertButton.Location = new System.Drawing.Point(135, 233);
+            this.insertButton.Name = "insertButton";
+            this.insertButton.Size = new System.Drawing.Size(75, 23);
+            this.insertButton.TabIndex = 0;
+            this.insertButton.Text = "Insert";
+            this.insertButton.UseVisualStyleBackColor = true;
+            this.insertButton.Click += new System.EventHandler(this.insertButton_Click);
+            // 
+            // regText
+            // 
+            this.regText.Location = new System.Drawing.Point(257, 76);
+            this.regText.Name = "regText";
+            this.regText.Size = new System.Drawing.Size(100, 20);
+            this.regText.TabIndex = 1;
+            // 
+            // nameText
+            // 
+            this.nameText.Location = new System.Drawing.Point(257, 103);
+            this.nameText.Name = "nameText";
+            this.nameText.Size = new System.Drawing.Size(100, 20);
+            this.nameText.TabIndex = 2;
+            // 
+            // classText
+            // 
+            this.classText.Location = new System.Drawing.Point(257, 130);
+            this.classText.Name = "classText";
+            this.classText.Size = new System.Drawing.Size(100, 20);
+            this.classText.TabIndex = 3;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(282, 233);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(75, 23);
+            this.deleteButton.TabIndex = 4;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(694, 233);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(62, 23);
+            this.searchButton.TabIndex = 5;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(135, 193);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(222, 23);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.SkyBlue;
+            this.label1.Location = new System.Drawing.Point(132, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Reg #";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // updateButton
+            // 
+            this.updateButton.Location = new System.Drawing.Point(208, 262);
+            this.updateButton.Name = "updateButton";
+            this.updateButton.Size = new System.Drawing.Size(75, 23);
+            this.updateButton.TabIndex = 10;
+            this.updateButton.Text = "Update";
+            this.updateButton.UseVisualStyleBackColor = true;
+            this.updateButton.Click += new System.EventHandler(this.updateButton_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.SkyBlue;
+            this.label2.Location = new System.Drawing.Point(132, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Name";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.SkyBlue;
+            this.label3.Location = new System.Drawing.Point(132, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Class";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // idText
+            // 
+            this.idText.Location = new System.Drawing.Point(257, 50);
+            this.idText.Name = "idText";
+            this.idText.Size = new System.Drawing.Size(100, 20);
+            this.idText.TabIndex = 15;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.SkyBlue;
+            this.label4.Location = new System.Drawing.Point(132, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "ID";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(443, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(304, 146);
+            this.dataGridView1.TabIndex = 17;
+            // 
+            // idSearchText
+            // 
+            this.idSearchText.Location = new System.Drawing.Point(560, 235);
+            this.idSearchText.Name = "idSearchText";
+            this.idSearchText.Size = new System.Drawing.Size(128, 20);
+            this.idSearchText.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.SkyBlue;
+            this.label5.Location = new System.Drawing.Point(440, 233);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Enter Id";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.idSearchText);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.idText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.updateButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.classText);
+            this.Controls.Add(this.nameText);
+            this.Controls.Add(this.regText);
+            this.Controls.Add(this.insertButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button insertButton;
+        private System.Windows.Forms.TextBox regText;
+        private System.Windows.Forms.TextBox nameText;
+        private System.Windows.Forms.TextBox classText;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button updateButton;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox idText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox idSearchText;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ dbCRUD/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace dbCRUD
+{
+    public partial class Form1 : Form
+    {
+        SqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        SqlConnection conn = new SqlConnection("Data Source=SUPOLARIS\\SQLEXPRESS;Initial Catalog=studentDb;Integrated Security=True");
+        private void insertButton_Click(object sender, EventArgs e)
+        { 
+            conn.Open();
+
+            string myReg = regText.Text;
+            cmd = new SqlCommand("SELECT * FROM dbCrud WHERE reg=@REG", conn);
+            cmd.Parameters.Add("REG", regText.Text);
+            cmd.ExecuteNonQuery();
+            if (regText.Text.Equals(myReg))
+            {
+                MessageBox.Show("User with registration number: " + myReg +  " Already exists");
+
+            }
+            else
+            {
+                cmd = new SqlCommand("INSERT INTO dbCrud (id, reg, name, class) VALUES (@ID, @REG, @NAME, @CLASS)", conn);
+                cmd.Parameters.Add("ID", idText.Text);
+                cmd.Parameters.Add("REG", regText.Text);
+                cmd.Parameters.Add("NAME", nameText.Text);
+                cmd.Parameters.Add("CLASS", classText.Text);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data Stored");
+            }
+            conn.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            regText.Text = null;
+            nameText.Text = null;
+            classText.Text = null;
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("DELETE dbCrud WHERE Id=@ID", conn);
+            cmd.Parameters.Add("ID", idText.Text);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("User Deleted");
+
+        }
+
+        private void updateButton_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("UPDATE dbCrud SET reg=@REG, name=@NAME, class=@CLASS WHERE Id=@ID", conn);
+            cmd.Parameters.Add("ID", idText.Text);
+            cmd.Parameters.Add("REG", regText.Text);
+            cmd.Parameters.Add("NAME", nameText.Text);
+            cmd.Parameters.Add("CLASS", classText.Text);
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Data Updated");
+        }
+
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            cmd = new SqlCommand("SELECT * FROM dbCrud WHERE Id=@ID", conn);
+            cmd.Parameters.Add("ID", idSearchText.Text);
+            cmd.ExecuteNonQuery();
+            SqlDataAdapter myAdapter = new SqlDataAdapter(cmd);
+            DataTable myDataTable = new DataTable();
+            myAdapter.Fill(myDataTable);
+
+            dataGridView1.DataSource = myDataTable;
+            conn.Close();
+        }
+    }
+}
